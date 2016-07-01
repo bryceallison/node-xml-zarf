15719d758d66c1606d99d6a0cd108279b30bcaba
Andrew Plotkin <zarf@Zarfslab.local>
2016-07-01 19:23:21 -0400

    Export the usertag module.

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 'use strict';
 
+const usertag = require('./usertag.js');
 const xmlparse = require('./xmlparse.js');
 const xmlwrite = require('./xmlwrite.js');
 
 exports.xmlparse = xmlparse;
 exports.xmlwrite = xmlwrite;
+exports.usertag = usertag;
