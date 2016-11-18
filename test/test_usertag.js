a8127e148f2aa214ef33367b2b1aee1401f81f6c
Andrew Plotkin <zarf@Zarfslab.local>
2016-11-18 14:58:51 -0500

    UserTag objects can have attributes now.

--- test/test_usertag.js
+++ test/test_usertag.js
@@ -19,7 +19,25 @@ test('usertag simple tags', function(t) {
         t.end();
 });
 
-test('usertag tag children', function(t) {
+test('usertag nameless tags', function(t) {
+        t.assert(tagequal(new UserTag(), new UserTag()));
+        t.assert(tagequal(new UserTag(['x']), new UserTag(['x'])));
+        t.assert(!tagequal(new UserTag(['x']), new UserTag(['y'])));
+        t.assert(!tagequal(new UserTag(['x']), new UserTag([])));
+        t.assert(!tagequal(new UserTag('x', []), new UserTag([])));
+        t.end();
+});
+
+test('usertag empty attrs', function(t) {
+        t.assert(tagequal(new UserTag('foo'), new UserTag('foo', [])));
+        t.assert(tagequal(new UserTag('foo'), new UserTag('foo', {}, [])));
+        t.assert(tagequal(new UserTag('foo', undefined, undefined), new UserTag('foo', {}, [])));
+        t.assert(tagequal(new UserTag('foo'), new UserTag('foo', null, [])));
+        t.assert(tagequal(new UserTag('foo'), new UserTag('foo', undefined, [])));
+        t.end();
+});
+
+test('usertag simple children', function(t) {
         t.assert(tagequal(
                 new UserTag('foo', []), 
                 new UserTag('foo')
@@ -55,6 +73,62 @@ test('usertag tag children', function(t) {
         t.assert(!tagequal(
                 new UserTag('foo', [ new UserTag('bar') ]), 
                 new UserTag('foo', [ new UserTag('zaz') ])
+        ));
+        t.end();
+});
+
+test('usertag attrs and children', function(t) {
+        t.assert(tagequal(
+                new UserTag('foo', 'text'), 
+                new UserTag('foo', {}, 'text')
+        ));
+        t.assert(tagequal(
+                new UserTag('foo', 'text'), 
+                new UserTag('foo', {}, ['text'])
+        ));
+        t.assert(tagequal(
+                new UserTag('foo', {}), 
+                new UserTag('foo', {})
+        ));
+        t.assert(tagequal(
+                new UserTag('foo', { bar:'xx' }), 
+                new UserTag('foo', { bar:'xx' })
+        ));
+        t.assert(!tagequal(
+                new UserTag('foo'), 
+                new UserTag('foo', { bar:'xx' }, [])
+        ));
+        t.assert(!tagequal(
+                new UserTag('foo', { bar:'xx' }), 
+                new UserTag('foo', { bar:'yy' })
+        ));
+        t.assert(!tagequal(
+                new UserTag('foo', { bar:'xx' }), 
+                new UserTag('foo', { zork:'xx' })
+        ));
+        t.assert(tagequal(
+                new UserTag('foo', { bar:'xx' }, 'text'), 
+                new UserTag('foo', { bar:'xx' }, 'text')
+        ));
+        t.assert(tagequal(
+                new UserTag('foo', { bar:'xx' }, 'text'), 
+                new UserTag('foo', [ { key:'bar', val:'xx' } ], 'text')
+        ));
+        t.assert(tagequal(
+                new UserTag('foo', { bar:'xx' }, [ 'text' ]), 
+                new UserTag('foo', [ { key:'bar', val:'xx' } ], 'text')
+        ));
+        t.assert(!tagequal(
+                new UserTag('foo', [ { key:'bar', val:'xx' }, { key:'zog', val:'yy' } ], []),
+                new UserTag('foo', [ { key:'bar', val:'xx' } ], [])
+        ));
+        t.assert(!tagequal(
+                new UserTag('foo', [ { key:'bar', val:'xx' }, { key:'zog', val:'yy' } ], []),
+                new UserTag('foo', [ { key:'zog', val:'yy' }, { key:'bar', val:'xx' } ], [])
+        ));
+        t.assert(tagequal(
+                new UserTag('foo', [ { key:'bar', val:'xx' }, { key:'zog', val:'yy' } ], []),
+                new UserTag('foo', [ { key:'bar', val:'xx' }, { key:'zog', val:'yy' } ], [])
         ));
         t.end();
 });
--- usertag.js
+++ usertag.js
@@ -1,17 +1,56 @@
 'use strict';
 
-function UserTag(tagname, children)
+/* The UserTag constructor is annoyingly flexible:
+   
+   new UserTag('div')
+   new UserTag('div', 'text') // 'text' is a child
+   new UserTag('div', ['text']) // 'text' is a child
+   new UserTag('div', { x:'y' }) // x:y is an attribute
+   new UserTag('div', { x:'y' }, 'text')
+   new UserTag(['text']) // no tag name, 'text'is a child
+   new UserTag('div', [{key:'x', val:'y'}], ['text'])
+*/
+
+
+function UserTag(tagname, attrs, children)
 {
-    if (Array.isArray(tagname)) {
+    if (arguments.length === 1 && Array.isArray(tagname)) {
         children = tagname;
         tagname = undefined;
     }
+    else if (arguments.length === 2) {
+        if (Array.isArray(attrs) || typeof(attrs) == 'string') {
+            children = attrs;
+            attrs = undefined;
+        }
+    }
+
     if (typeof(children) == 'string') {
         children = [ children ];
     }
 
     this.tagname = tagname;
     this.children = children;
+
+    this.attrlist = [];
+    this.attrmap = {};
+    if (!attrs) {
+        /* do nothing */
+    }
+    else if (Array.isArray(attrs)) {
+        for (var ix=0; ix<attrs.length; ix++) {
+            var obj = attrs[ix];
+            this.attrlist.push(obj);
+            this.attrmap[obj.key] = obj.val;
+        }
+    }
+    else {
+        for (var key in attrs) {
+            var val = attrs[key];
+            this.attrlist.push({ key:key, val:val });
+            this.attrmap[key] = val;
+        }
+    }
 }
 
 function tagequal(tag1, tag2)
@@ -26,6 +65,15 @@ function tagequal(tag1, tag2)
 
     if (tag1.tagname != tag2.tagname)
         return false;
+
+    if (tag1.attrlist.length != tag2.attrlist.length)
+        return false;
+    for (var ix=0; ix<tag1.attrlist.length; ix++) {
+        var at1 = tag1.attrlist[ix];
+        var at2 = tag2.attrlist[ix];
+        if (at1.key != at2.key || at1.val != at2.val)
+            return false;
+    }
 
     var has1 = (tag1.children && tag1.children.length);
     var has2 = (tag2.children && tag2.children.length);
